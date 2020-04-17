2cede9ffd12e1a1534cecefb5b584b9bc53c034d
homer li <homer.lee.0508@gmail.com>
2020-04-16 20:05:33 -0700

    get link back from title

--- module/google_search.py
+++ module/google_search.py
@@ -9,17 +9,18 @@ class get_news:
     def google_news(self, query):
         query = query.replace(" ", "+")
         url = f"https://google.ca/search?q={query}&num=30"
-        print(url)
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         title = soup.find_all("div", class_="BNeawe vvjwJb AP7Wnd")
-        link = soup.find_all("a")
-        return title
+        link = soup.find_all("a", class_="BVG0Nb")
+        return link
+
+
+def get_url(str):
+    return str.split("/url?q=", 1)[1].split("&sa", 1)[0]
 
 
 first = get_news()
 result = first.google_news("covid 19")
-print(result.__len__())
-
 for title in result:
-    print(title)
+    print(get_url(title.attrs['href']))
